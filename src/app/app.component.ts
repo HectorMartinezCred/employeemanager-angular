ac97992f63238b3f030bdd8f43a1302fe973e6d0
HectorMartinezCred <hector.martinez.cred@outlook.com>
2022-08-30 13:42:46 -0500

    UI Modal Logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,4 +27,23 @@ export class AppComponent implements OnInit{
       }
     );
   }
+
+  public onOpenModal(mode: string): void {
+    const container = document.getElementById("main-container");
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'update') {
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
 }
